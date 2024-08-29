25774dd879448f1361d52754646abe2571fefe4c
Pratishruti Sahoo <pratishrutisahoo7@gmail.com>
2024-08-29 10:44:45 +0530

    ScopeInJs added

--- Scope.js
+++ Scope.js
@@ -64,3 +64,19 @@ function exampleConst() {
 }
 
 exampleConst();
+
+
+
+function scope(){
+    var a =10;
+    let b = 20;
+    const c = 30;
+
+        console.log(a); //10
+        console.log(b); //20
+        console.log(c); //30
+}
+console.log(a); // ReferenceError: a is not defined
+console.log(b); // ReferenceError: b is not defined
+console.log(c); // ReferenceError: c is not defined
+scope();
